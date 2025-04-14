5ec750578cb7758e418319c583baf5413b230d08
ArthurHastings <horialitan@yahoo.com>
2025-04-14 22:13:07 +0300

    zaza cox

--- mlflow_func.py
+++ mlflow_func.py
@@ -1,17 +1,20 @@
+import mlflow.pyfunc
 import tensorflow as tf
-import mlflow
 import pickle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 class SpamModelWrapper(mlflow.pyfunc.PythonModel):
     def load_context(self, context):
-        model_path = context.artifacts["model"]
-        tokenizer_path = context.artifacts["tokenizer"]
-
-        self.model = tf.keras.models.load_model(model_path)
-        with open(tokenizer_path, "rb") as f:
+        self.model = tf.keras.models.load_model(context.artifacts["model"])
+        with open(context.artifacts["tokenizer"], "rb") as f:
             self.tokenizer = pickle.load(f)
+        self.max_len = 100
 
     def predict(self, context, model_input):
-        sequences = self.tokenizer.texts_to_sequences(model_input["text"])
-        # padding etc. here if needed
-        return self.model.predict(sequences)
+        texts = model_input["text"].tolist()
+        sequences = self.tokenizer.texts_to_sequences(texts)
+        padded = pad_sequences(sequences, maxlen=self.max_len)
+        return self.model.predict(padded)
+
+
+# mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root file:///C:/Users/User/Desktop/PythonAICourse/mlflow_artifacts --host 0.0.0.0 --port 5000
--- mlflow_log.py
+++ mlflow_log.py
@@ -1,16 +1,12 @@
 import mlflow.pyfunc
-import os
-import pickle
-import tensorflow as tf
+
 from mlflow_func import SpamModelWrapper
 
-# No need to set tracking URI here if using ngrok directly
-mlflow.set_tracking_uri("https://4e64-2a02-2f04-530c-c000-511d-87f7-111d-1a6f.ngrok-free.app")
-# Assuming MLflow server is running with ngrok on the GitHub Actions environment
+mlflow.set_tracking_uri("https://98f7-2a02-2f04-530c-c000-511d-87f7-111d-1a6f.ngrok-free.app")
 
 artifacts = {
-    "model": "spam_model/spam_model.keras",
-    "tokenizer": "spam_model/tokenizer_spam.pkl"
+    "model": "spam_model.keras",
+    "tokenizer": "tokenizer_spam.pkl"
 }
 
 mlflow.pyfunc.log_model(
